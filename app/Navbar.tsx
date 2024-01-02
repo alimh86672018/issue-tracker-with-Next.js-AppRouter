a8455eb187f9b7a36d55d03955525a1f4a73f0e5
alimh86672018 <alimh86672018@gmail.com>
2024-01-01 23:35:42 -0800

    Fix avatar not loading

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -62,6 +62,7 @@ const Navbar = () => {
                     size="2"
                     radius="full"
                     className="cursor-pointer"
+                    referrerPolicy="no-referrer"
                   />
                 </DropdownMenu.Trigger>
                 <DropdownMenu.Content>
