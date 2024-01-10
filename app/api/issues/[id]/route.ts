59326fabb73205152b9dbda4090ee217056f1f17
alimh86672018 <alimh86672018@gmail.com>
2024-01-09 21:31:09 -0800

    Inhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { IssueSchema } from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -12,10 +12,20 @@ export async function PATCH(
   if (!session) return NextResponse.json({}, { status: 401 });
 
   const body = await request.json();
-  const validation = IssueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { title, description, assignedToUserId } = body;
+
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid user." }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -27,8 +37,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
 
--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -5,6 +5,21 @@ export const IssueSchema = z.object({
   description: z.string().min(1, "Description is required"),
 });
 
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is required").max(255).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required")
+    .max(255)
+    .nullable()
+    .optional(),
+});
+
 export const userSchema = z.object({
   name: z.string().min(3),
   email: z.string().email(),
